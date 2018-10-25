1bef7e82e1390c86274c0a4d9527e5032513dab4
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2018-10-25 09:42:13 -0700

    images

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+
+//$('h1').css({'background-color': '#FFFFCC'});
+
+var randomNumber;
+var win;
+var lose;
+for (var i = 0; i < 12; i++);
+
+//1. a variable named "numberBtn" equal to $<"Button">;
+var crystalBtn = $("Button");
+
+
+
+
+
+// four Crystals displayed as buttons
+// All Crystals each has hidden value
+// player showed random number at the start of the game 
+// each Crystals contains random number between 1 and 12
+// crystal should generate new random number every time after win or lose
+// when clicking crystal it should add to the previous result
+// Until it equals random result = win
+// Until exceeds random result = lose
+// Game restarts when player loses
+// Each player has new hidden value
+
