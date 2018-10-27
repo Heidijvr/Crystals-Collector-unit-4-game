e4fd99b920327d4e79fcbb2c98359073d8334f92
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2018-10-26 23:57:47 -0700

    Almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,62 @@
+var crystalValues = [];
+var total;
+var target;
+var winCount = 0;
+var lossCount = 0;
+
+var resetGame = function() {
+    for (var i = 0; i < 4; i++) {
+        crystalValues[i] = Math.floor(Math.random() * 12) + 1;
+    }
+    total = 0;
+    target = Math.floor(Math.random() * 102) + 19;
+    document.getElementById("randomNumber").innerHTML = target;
+    document.getElementById("wins").innerHTML = winCount;
+    document.getElementById("losses").innerHTML = lossCount;
+    document.getElementById("currentTotal").innerHTML = total; 
+}
+
+resetGame();
 
 
-//$('h1').css({'background-color': '#FFFFCC'});
+document.getElementById("diamond").onclick = function() {
+   addCrystalValueToTotal(0);
+}
 
-var randomNumber;
-var win;
-var lose;
-for (var i = 0; i < 12; i++);
+document.getElementById("garnet").onclick = function () {
+    addCrystalValueToTotal(1);
+}
 
-//1. a variable named "numberBtn" equal to $<"Button">;
-var crystalBtn = $("Button");
+document.getElementById("peridot").onclick = function () {
+    addCrystalValueToTotal(2);
+}
+
+document.getElementById("ruby").onclick = function () {
+    addCrystalValueToTotal(3);
+}
+
+var addCrystalValueToTotal = function(crystalIndex) {
+    document.getElementById("youWinLose").innerHTML = "";
+    total += crystalValues[crystalIndex];
+    document.getElementById("currentTotal").innerHTML = total;
+    console.log("totalValue: ", total, "crystal value: ", crystalValues[crystalIndex]);
+    if (total === target) {        
+        //win
+        winCount++;
+        document.getElementById("youWinLose").innerHTML = "You Win!"
+        resetGame();
+    } else if (total > target) {
+       //lose 
+       lossCount++; 
+       document.getElementById("youWinLose").innerHTML = "You Lose!"
+       resetGame();
+    }
+
+    document.getElementById("currentTotal").innerHTML = total;
+}
 
 
 
 
 
-// four Crystals displayed as buttons
-// All Crystals each has hidden value
-// player showed random number at the start of the game 
-// each Crystals contains random number between 1 and 12
-// crystal should generate new random number every time after win or lose
-// when clicking crystal it should add to the previous result
-// Until it equals random result = win
-// Until exceeds random result = lose
-// Game restarts when player loses
-// Each player has new hidden value
 
